b264862e48d0abd81122c88e6bee86e0a6971468
Ankit Goel <goelankit@protonmail.com>
2018-06-26 12:42:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# local_server
+
+A description of this package.
---
+++ Sources/local_server/main.swift
@@ -0,0 +1,23 @@
+import Embassy
+import Foundation
+
+let loop = try! SelectorEventLoop(selector: try! KqueueSelector())
+let server = DefaultHTTPServer(eventLoop: loop, port: 8080) {
+    (
+        environ: [String: Any],
+        startResponse: ((String, [(String, String)]) -> Void),
+        sendBody: ((Data) -> Void)
+    ) in
+    // Start HTTP response
+    startResponse("200 OK", [])
+    let pathInfo = environ["PATH_INFO"]! as! String
+    sendBody(Data("the path you're visiting is \(pathInfo.debugDescription)".utf8))
+    // send EOF
+    sendBody(Data())
+}
+
+// Start HTTP server to listen on the port
+try! server.start()
+
+// Run event loop
+loop.runForever()
