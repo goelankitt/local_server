b31a2c90220d9de8ab9b2c4a65bc13986399c3bc
Ankit Goel <goelankit@protonmail.com>
2018-06-26 13:08:10 +0530

    Send response from file

--- Sources/local_server/main.swift
+++ Sources/local_server/main.swift
@@ -11,8 +11,31 @@ let server = DefaultHTTPServer(eventLoop: loop, port: 8080) {
     // Start HTTP response
     startResponse("200 OK", [])
     let pathInfo = environ["PATH_INFO"]! as! String
-    sendBody(Data("the path you're visiting is \(pathInfo.debugDescription)".utf8))
-    // send EOF
+    if pathInfo == "/hello" {
+        let file = "file.txt"
+        if let dir = FileManager.default.urls(for: .documentDirectory,
+        in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(file)
+            // Write to a file in documents directory
+            // do {
+            //     try text.write(to: fileURL, atomically: false, encoding: .utf8)
+            // }
+            // catch {/* error handling here */}
+
+            // Read from a file in documents directory
+            do {
+                let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+                sendBody(Data(text2.utf8))
+            }
+            catch {
+                print("Error while reading file")
+                sendBody(Data("Error reading file".utf8))
+            }
+        }
+        
+    } else {
+        sendBody(Data("the path you're visiting is \(pathInfo.debugDescription)".utf8))
+    }
     sendBody(Data())
 }
 
